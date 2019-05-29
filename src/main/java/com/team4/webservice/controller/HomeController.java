702c7fe608e2c7b70d60421380f4a060dad507c4
rak <sonwonrak@gmail.com>
2019-05-29 17:09:06 +0900

    first commit

---
+++ src/main/java/com/team4/webservice/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.team4.webservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/main")
+	public String main(Model model) {
+		
+		return "main";
+	}
+	
+
+}
