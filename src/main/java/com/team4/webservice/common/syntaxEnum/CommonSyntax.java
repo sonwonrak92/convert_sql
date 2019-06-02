e4c1e249387c4e3ee34810cfdceaf9420c9c58d9
rak <sonwonrak@gmail.com>
2019-06-02 15:32:47 +0900

    CommonSyntax 추가

--- src/main/java/com/team4/webservice/common/syntaxEnum/CommonSyntax.java
+++ src/main/java/com/team4/webservice/common/syntaxEnum/CommonSyntax.java
@@ -2,29 +2,29 @@ package com.team4.webservice.common.syntaxEnum;
 
 public enum CommonSyntax {
 
-	   SELECT,
-	   FROM ,
-	   WHERE,
+	   SELECT("SELECT"),
+	   FROM("FROM") ,
+	   WHERE("WHERE"),
 	   IS_NOT_NULL("IS NOT NULL"),
-	   IS_NULL,
-	   DISTICT,
-	   WITH,
-	   UNION_ALL,
-	   UNION,
-	   LIKE,
-	   HAVING,
-	   GROUP_BY,
-	   ORDER_BY,
-	   DESC,
-	   ASC,
-	   BETWEEN,
-	   AND,
-	   OR,
-	   NOT;
+	   IS_NULL("IS NULL"),
+	   DISTINCT("DISTINCT"),
+	   WITH("WITH"),
+	   UNION_ALL("UNION ALL"),
+	   UNION("UNION"),
+	   LIKE("LIKE"),
+	   HAVING("HAVING"),
+	   GROUP_BY("GROUP BY"),
+	   ORDER_BY("ORDER BY"),
+	   DESC("DESC"),
+	   ASC("ASC"),
+	   BETWEEN("BETWEEN"),
+	   AND("AND"),
+	   OR("OR"),
+	   NOT("NOT");
 	
 
 	private String str;
-
+	
 	private CommonSyntax(String str) {
 		this.str = str;
 	}
