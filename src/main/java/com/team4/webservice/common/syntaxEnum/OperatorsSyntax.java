ac770aedf31eb1b981701068e92ca9e5289ca824
bbubbush <bbubbush@gmail.com>
2019-06-02 15:32:03 +0900

    특수문자 enum 수정

--- src/main/java/com/team4/webservice/common/syntaxEnum/OperatorsSyntax.java
+++ src/main/java/com/team4/webservice/common/syntaxEnum/OperatorsSyntax.java
@@ -1,5 +1,32 @@
 package com.team4.webservice.common.syntaxEnum;
 
-public enum OperatorsSyntax {
+import com.sun.tools.corba.se.idl.constExpr.Equal;
 
+public enum OperatorsSyntax {
+    EQUAL("="),
+    NOT("NOT"),
+    LIKE("LIKE"),
+    AND("AND"),
+    OR("OR"),
+    BETWEEN("BETWEEN"),
+    IS_NULL("IS NULL"),
+    NOT_BETWEEN("NOT BETWEEN"),
+    NOT_IS("NOT IS"),
+    IS_NOT_NULL("IS NOT NULL"),
+    EXCLAMATION_POINT("!"),
+    LESS_THAN("<"),
+    GREATER_THAN(">"),
+    LESS_THAN_EQUAL("<="),
+    GREATER_THAN_EQUAL(">="),
+    NOT_EQUAL_TYPE1("!="),
+    NOT_EQUAL_TYPE2("^="),
+    NOT_EQUAL_TYPE3("<>"),
+    ;
+
+    String character;
+
+    OperatorsSyntax(String character) {
+        this.character = character;
+
+    }
 }
