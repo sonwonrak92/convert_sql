85fb681cfe4601f75ca76282f3d69d22ef47ffb5
bbubbush <bbubbush@gmail.com>
2019-06-02 15:16:43 +0900

    특수문자 enum 생성

--- src/main/java/com/team4/webservice/common/syntaxEnum/SpecialCharactersSyntax.java
+++ src/main/java/com/team4/webservice/common/syntaxEnum/SpecialCharactersSyntax.java
@@ -1,5 +1,12 @@
 package com.team4.webservice.common.syntaxEnum;
 
 public enum SpecialCharactersSyntax {
+    PERCENT_SIGN("%"),
+    UNDER_BAR("_");
 
+    String character;
+
+    SpecialCharactersSyntax(String character) {
+        this.character = character;
+    }
 }
