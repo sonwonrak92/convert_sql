43a9b67d529f8889b5d77920db7bfe147df229fc
bbubbush <bbubbush@gmail.com>
2019-06-02 15:19:32 +0900

    특수문자 enum 수정

--- src/main/java/com/team4/webservice/common/syntaxEnum/AnsiSyntax.java
+++ src/main/java/com/team4/webservice/common/syntaxEnum/AnsiSyntax.java
@@ -1,5 +1,16 @@
 package com.team4.webservice.common.syntaxEnum;
 
 public enum AnsiSyntax {
+    INNER_JOIN("INNER JOIN"),
+    OUTER_JOIN("OUTER JOIN"),
+    LEFT_OUTER_JOIN("LEFT INNER JOIN"),
+    RIGHT_OUTER_JOIN("RIGHT INNER JOIN"),
+    PULL_OUTER_JOIN("PULL INNER JOIN"),
+    USING("USING");
 
+    String character;
+
+    AnsiSyntax(String character) {
+        this.character = character;
+    }
 }
