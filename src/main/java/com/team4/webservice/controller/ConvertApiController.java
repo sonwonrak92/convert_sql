4e0e55ee1ab92f9faa1d9d09d4e50e73e792b281
bbubbush <bbubbush@gmail.com>
2019-06-05 23:45:55 +0900

    api 컨트롤러 생성

---
+++ src/main/java/com/team4/webservice/controller/ConvertApiController.java
@@ -0,0 +1,23 @@
+package com.team4.webservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/api")
+public class ConvertApiController {
+
+    /** ANSI SQL convert to Oracle SQL
+     *
+     * @param param
+     * @return T^T
+     * */
+    @GetMapping("/ansi/oracle")
+    public Map<String, Object> ansiToOracle(@RequestBody Map<String, Object> param) {
+
+        return null;    // TODO 임시
+    }
+
+}
