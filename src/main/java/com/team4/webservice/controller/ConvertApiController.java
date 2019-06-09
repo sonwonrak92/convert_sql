5980a5707bfb336e4e80a42d55ab998b0f97458b
bbubbush <bbubbush@gmail.com>
2019-06-09 15:18:28 +0900

    테스트

--- src/main/java/com/team4/webservice/controller/ConvertApiController.java
+++ src/main/java/com/team4/webservice/controller/ConvertApiController.java
@@ -2,11 +2,13 @@ package com.team4.webservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class ConvertApiController {
 
     /** ANSI SQL convert to Oracle SQL
@@ -15,8 +17,8 @@ public class ConvertApiController {
      * @return T^T
      * */
     @GetMapping("/ansi/oracle")
-    public Map<String, Object> ansiToOracle(@RequestBody Map<String, Object> param) {
-
+    public Map<String, Object> ansiToOracle(@RequestBody String param) {
+        System.out.println(param);
         return null;    // TODO 임시
     }
 
--- src/main/java/com/team4/webservice/controller/HomeController.java
+++ src/main/java/com/team4/webservice/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.team4.webservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -10,7 +12,12 @@ public class HomeController {
 	
 	@GetMapping("/main")
 	public String main(Model model) {
-		
+
+		return "main";
+	}
+	@PostMapping("/test")
+	public String test(@RequestBody String str) {
+		System.out.println(str);
 		return "main";
 	}
 	
