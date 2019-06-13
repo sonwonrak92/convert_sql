a9483f5cb0e7a018d317e29bd79574f544bf0faf
rak <sonwonrak@gmail.com>
2019-06-13 20:07:56 +0900

    UI Main페이지 생성

--- src/main/java/com/team4/webservice/controller/HomeController.java
+++ src/main/java/com/team4/webservice/controller/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
 	
@@ -13,6 +12,7 @@ public class HomeController {
 		
 		return "main";
 	}
+	
 	
 
 }
