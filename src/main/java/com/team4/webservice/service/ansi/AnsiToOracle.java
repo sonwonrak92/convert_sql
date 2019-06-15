2172b46d5d7531f1c4c609ee5dd831c56f007b70
mike6321 <33277588+mike6321@users.noreply.github.com>
2019-06-15 16:16:15 +0900

    Merge branch 'master' of https://github.com/sonwonrak92/convert_sql
    # Conflicts:
    #	src/main/resources/templates/main.html

--- src/main/java/com/team4/webservice/service/ansi/AnsiToOracle.java
+++ src/main/java/com/team4/webservice/service/ansi/AnsiToOracle.java
@@ -20,11 +20,30 @@ public class AnsiToOracle implements Ansi{
 
     @Override
     public void attachStr() {
+        String str = "";
+        //2) char 배열으로 넘길때
+        char[] arr;
+        arr = str.toCharArray();
+
+        for(int i=0;i<arr.length;i++){
+            if(97<=arr[i] && arr[i] <=122)
+                arr[i] = (char)(arr[i]-32);
+        }
+
+        //1) string 값으로 넘길때
+        //str.toUpperCase();
 
     }
 
     @Override
     public void splitStr() {
 
+    }
+
+    @Override
+    public void removeSpaces() {
+        //1
+        String str = "";
+        str.replaceAll("\\p{Z}","");
     }
 }
