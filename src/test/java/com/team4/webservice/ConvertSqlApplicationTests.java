282c38d847e019ecffcdd28e76c7342bd5dfe709
bbubbush <bbubbush@gmail.com>
2019-06-23 12:23:26 +0900

    Merge branch 'master' of https://github.com/sonwonrak92/convert_sql
    # Conflicts:
    #	src/test/java/com/team4/webservice/ConvertSqlApplicationTests.java

--- src/test/java/com/team4/webservice/ConvertSqlApplicationTests.java
+++ src/test/java/com/team4/webservice/ConvertSqlApplicationTests.java
@@ -1,13 +1,12 @@
 package com.team4.webservice;
 
+import com.team4.webservice.common.QueryConvertUtil;
+import com.team4.webservice.service.ansi.AnsiToOracle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.team4.webservice.common.QueryConverUtil;
-import com.team4.webservice.service.ansi.AnsiToOracle;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,9 +21,9 @@ public class ConvertSqlApplicationTests {
 		String oldQuery = "SELECT     ID   AS \"사      번\" FROM EMP AS \"add  df\" INNER JOIN      DEP    AS \"345\" ON A.ID = B.ID WHERE A.ID > 1000 ;";
 		String newQuery;
 
-		newQuery = QueryConverUtil.replaceAllSingleSpace(oldQuery);
+		newQuery = QueryConvertUtil.replaceAllSingleSpace(oldQuery);
 
-		newQuery = QueryConverUtil.SetQueryText(newQuery,QueryConverUtil.GetQueryText(oldQuery));
+		newQuery = QueryConvertUtil.SetQueryText(newQuery,QueryConvertUtil.GetQueryText(oldQuery));
 
 		System.out.println(newQuery);
 
